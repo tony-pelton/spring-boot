d20cb0296d38e3b48729c5d0f93c4ce5ec31de54
Tony Pelton <tpelton@gmail.com>
2025-04-19 17:13:12 -0400

    *) consolidate scheduled processing

--- integration/src/main/java/com/dsrts/integration/service/SchedulerService.java
+++ integration/src/main/java/com/dsrts/integration/service/SchedulerService.java
@@ -16,8 +16,7 @@ public class SchedulerService {
     private final BookService bookService;
     private final CustomerService customerService;
 
-    @Scheduled(fixedDelay = 5000)
-    public void processBookWebMessages() {
+    private void processBookWebMessages() {
 
         int count = messageStore.messageGroupSize(ChannelConfiguration.BOOK_WEB_MESSAGE);
         if(0 < count) {
@@ -30,22 +29,7 @@ public class SchedulerService {
         }
     }
 
-    @Scheduled(fixedDelay = 5000)
-    public void processBookWarehouseMessages() {
-
-        int count = messageStore.messageGroupSize(ChannelConfiguration.BOOK_WAREHOUSE_MESSAGE);
-        if(0 < count) {
-            log.info("processBookWarehouseMessages() count={}", count);
-        }
-        for (int i = 0; i < count; i++) {
-            if (!bookService.processBookWarehouseMessage()) {
-                return;
-            }
-        }
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    public void processCustomerWebMessages() {
+    private void processCustomerWebMessages() {
 
         int count = messageStore.messageGroupSize(ChannelConfiguration.CUSTOMER_WEB_MESSAGE);
         if(0 < count) {
@@ -59,7 +43,44 @@ public class SchedulerService {
     }
 
     @Scheduled(fixedDelay = 5000)
-    public void processCustomerWarehouseMessages() {
+    public void processWebMessages() {
+
+        try {
+
+            processBookWebMessages();
+        } catch (Exception e) {
+
+            log.error("processWebMessages()",e);
+        }
+
+        try {
+            processCustomerWebMessages();
+
+        } catch (Exception e) {
+
+            log.error("processWebMessages()",e);
+        }
+
+    }
+
+    private void processBookWarehouseMessages() {
+
+        int count = messageStore.messageGroupSize(ChannelConfiguration.BOOK_WAREHOUSE_MESSAGE);
+
+        if(0 < count) {
+
+            log.info("processBookWarehouseMessages() count={}", count);
+        }
+        for (int i = 0; i < count; i++) {
+
+            if (!bookService.processBookWarehouseMessage()) {
+
+                return;
+            }
+        }
+    }
+
+    private void processCustomerWarehouseMessages() {
 
         int count = messageStore.messageGroupSize(ChannelConfiguration.CUSTOMER_WAREHOUSE_MESSAGE);
         if(0 < count) {
@@ -70,5 +91,26 @@ public class SchedulerService {
                 return;
             }
         }
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void processWarehouseMessages() {
+
+        try {
+
+            processBookWarehouseMessages();
+        } catch (Exception e) {
+
+            log.error("processWarehouseMessages()",e);
+        }
+
+        try {
+
+            processCustomerWarehouseMessages();
+        } catch (Exception e) {
+
+            log.error("processWarehouseMessages()",e);
+        }
+
     }
 }
